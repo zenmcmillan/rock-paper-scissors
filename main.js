ed49c88a9dca6362bdd7298d966090c5f128b0f9
Zen McMillan <zenmcmillan@gmail.com>
2023-08-02 10:54:48 -0600

    Change the subtitle to choose your fighter when the hard or classic buttons are clicked. It will also change it back to the original text when the change game button is clicked

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var classicButton = document.querySelector('.classic-button');
 var classicAndHardContainers = document.querySelector('.classic-and-hard');
 var hardButton = document.querySelector('.hard-button');
 var changeGameButton = document.querySelector('.change-game-button');
+var subtitle = document.querySelector('.subtitle');
 
 // event listeners
 
@@ -24,9 +25,11 @@ changeGameButton.addEventListener('click', function() {
 function goToGame() {
   classicAndHardContainers.classList.add('hidden');
   changeGameButton.classList.remove('hidden')
+  subtitle.innerText = 'Choose your fighter!'
 };
 
 function goBackToHomePage() {
   classicAndHardContainers.classList.remove('hidden');
   changeGameButton.classList.add('hidden');
+  subtitle.innerText = 'Choose your game!'
 };
